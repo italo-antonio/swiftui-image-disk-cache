ff6dff0d213b1b37c7af7fe84bf8f123c2e01898
Italo Barzola <digital36@inter.mx>
2025-09-04 09:28:34 -0500

    Initial Commit

---
+++ StoreImageDiskCache/StoreImageDiskCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreImageDiskCacheApp.swift
+//  StoreImageDiskCache
+//
+//  Created by Italo Barzola García on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreImageDiskCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoreImageDiskCacheTests/StoreImageDiskCacheTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoreImageDiskCacheTests.swift
+//  StoreImageDiskCacheTests
+//
+//  Created by Italo Barzola García on 4/9/25.
+//
+
+import Testing
+@testable import StoreImageDiskCache
+
+struct StoreImageDiskCacheTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
